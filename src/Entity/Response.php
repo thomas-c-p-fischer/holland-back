ebe34e3ea82412ef7cbb50bfafb81d0d3bba46c8
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-29 17:34:35 +0200

    creation des entity
    User, Question, Response, Riasec.
    Relation ManyToMany entre question et response.
    creation des fichiers pour mettre à jour la BDD à partir des requetes.

---
+++ src/Entity/Response.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResponseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResponseRepository::class)]
+class Response
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $weight = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Riasec.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RiasecRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RiasecRepository::class)]
+class Riasec
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $realiste = null;
+
+    #[ORM\Column]
+    private ?int $investigateur = null;
+
+    #[ORM\Column]
+    private ?int $artistique = null;
+
+    #[ORM\Column]
+    private ?int $social = null;
+
+    #[ORM\Column]
+    private ?int $entreprenant = null;
+
+    #[ORM\Column]
+    private ?int $conventionnel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRealiste(): ?int
+    {
+        return $this->realiste;
+    }
+
+    public function setRealiste(int $realiste): self
+    {
+        $this->realiste = $realiste;
+
+        return $this;
+    }
+
+    public function getInvestigateur(): ?int
+    {
+        return $this->investigateur;
+    }
+
+    public function setInvestigateur(int $investigateur): self
+    {
+        $this->investigateur = $investigateur;
+
+        return $this;
+    }
+
+    public function getArtistique(): ?int
+    {
+        return $this->artistique;
+    }
+
+    public function setArtistique(int $artistique): self
+    {
+        $this->artistique = $artistique;
+
+        return $this;
+    }
+
+    public function getSocial(): ?int
+    {
+        return $this->social;
+    }
+
+    public function setSocial(int $social): self
+    {
+        $this->social = $social;
+
+        return $this;
+    }
+
+    public function getEntreprenant(): ?int
+    {
+        return $this->entreprenant;
+    }
+
+    public function setEntreprenant(int $entreprenant): self
+    {
+        $this->entreprenant = $entreprenant;
+
+        return $this;
+    }
+
+    public function getConventionnel(): ?int
+    {
+        return $this->conventionnel;
+    }
+
+    public function setConventionnel(int $conventionnel): self
+    {
+        $this->conventionnel = $conventionnel;
+
+        return $this;
+    }
+}
