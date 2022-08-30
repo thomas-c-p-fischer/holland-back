5c3b4a8d3cbb563ee4f0bb290fdaea8c8e4e1c47
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-30 14:29:21 +0200

    Revert "VF-authentification"

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
