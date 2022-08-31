297c80a723335602358857d1ffe43ca90234e163
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-31 09:59:33 +0200

    modifications de la base de données => requêtes

--- holland_back_response.sql
+++ holland_back_response.sql
@@ -1,5 +1,5 @@
-INSERT INTO `holland-back`.response (id, name, weight) VALUES (1, '"j''aime"', 1);
-INSERT INTO `holland-back`.response (id, name, weight) VALUES (2, '"j''aime pas"', 0);
-INSERT INTO `holland-back`.response (id, name, weight) VALUES (3, '"faible"', 0);
-INSERT INTO `holland-back`.response (id, name, weight) VALUES (4, '"moyen"', 0);
-INSERT INTO `holland-back`.response (id, name, weight) VALUES (5, '"fort"', 1);
+INSERT INTO `holland-back`.response (id, name, weight) VALUES (1, 'j''aime', 1);
+INSERT INTO `holland-back`.response (id, name, weight) VALUES (2, 'j''aime pas', 0);
+INSERT INTO `holland-back`.response (id, name, weight) VALUES (3, 'faible', 0);
+INSERT INTO `holland-back`.response (id, name, weight) VALUES (4, 'moyen', 0);
+INSERT INTO `holland-back`.response (id, name, weight) VALUES (5, 'fort', 1);
