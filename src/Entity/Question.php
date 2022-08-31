d85985b762d6a478e16f84e3e566def74bc99332
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-31 17:08:27 +0200

    EasyAdmin complété
    Les User : on peut ajouter des users qui vont se retrouver implémenté en BDD et dont le mot de passe sera chiffré
    Les questions : on peut modifier l'image associer à une question mais on ne peut ni ajouter ni supprimer une question.
    Et seul les admin peuvent faire ça.

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -118,4 +118,10 @@ class Question
 
         return $this;
     }
+
+    //Ajout de cette méthode pour pouvoir écrire la question
+    public function getQuestion()
+    {
+        return $this->getText();
+    }
 }
